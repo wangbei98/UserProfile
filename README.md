142db029e3010a5e583c9b1f97a4a17c1d3bab4e
bellick <beiwang121@163.com>
2019-05-21 22:53:40 +0800

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # UserProfile
+
+a website, very po , based on Django
+
+## Functions
+
+show  User portrait of certain app or website 
+
+## Rendering
+
+![](http://ww2.sinaimg.cn/large/006tNc79ly1g39btbc2fqj30jb0axt9j.jpg)
--- echarts/views.py
+++ echarts/views.py
@@ -4,22 +4,24 @@ import math
 from django.http import HttpResponse
 from django.template import loader
 from pyecharts import Line3D,Pie
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 import pandas as pd
 import numpy as np
+from django.views.decorators.csrf import csrf_exempt
+
 
 # from pyecharts.constants import DEFAULT_HOST # 这句会报错
 REMOTE_HOST = 'http://chfw.github.io/jupyter-echarts/echarts'#加上这句
 
-def charts(request):
-    template = loader.get_template('charts/pyecharts.html')
-    l3d = line3d()
-    context = dict(
-		myechart=l3d.render_embed(),
-		host=REMOTE_HOST,
-		script_list=l3d.get_js_dependencies()
-    )
-    return HttpResponse(template.render(context, request))
+# def charts(request):
+#     template = loader.get_template('charts/pyecharts.html')
+#     l3d = line3d()
+#     context = dict(
+# 		myechart=l3d.render_embed(),
+# 		host=REMOTE_HOST,
+# 		script_list=l3d.get_js_dependencies()
+#     )
+#     return HttpResponse(template.render(context, request))
 def line3d():
 	_data = []
 	for t in range(0, 25000):
@@ -37,23 +39,29 @@ def line3d():
 			   is_grid3D_rotate=True, grid3D_rotate_speed=180)
 	return line3d
 
-# app_tab = pd.read_csv('../data/appTab.txt',encoding='gb18030',sep='|',names=['app_id','name','male','female','_24','25_30','31_35','36_40','40_'])
-# app_tab = app_tab.set_index('app_id')
-# user_data = pd.read_csv('../data/userdata_new.txt',sep='|',names = ['tel','app_id','date','time'])
+app_tab = pd.read_csv('/Users/bellick/MyProjects/UserProfile/data/appTab.txt',encoding='gb18030',sep='|',names=['app_id','name','male','female','_24','25_30','31_35','36_40','40_'])
+app_tab = app_tab.set_index('app_id')
+user_data = pd.read_csv('/Users/bellick/MyProjects/UserProfile/data/userdata_new.txt',sep='|',names = ['tel','app_id','date','time'])
 
-# def charts(request):
-# 	if  request.method == "POST":
-# 		Id = int(request.POST.get('app_id'))
-#
-#     template = loader.get_template('charts/pyecharts.html')
-#     l3d = line3d()
-#     context = dict(
-# 		myechart=l3d.render_embed(),
-# 		host=REMOTE_HOST,
-# 		script_list=l3d.get_js_dependencies()
-#     )
-#     return HttpResponse(template.render(context, request))
 
+@csrf_exempt
+def charts(request):
+	if  request.method == "POST":
+		app_id = int(request.POST.get('app_id'))
+		template = loader.get_template('charts/pyecharts.html')
+		gender_pie = gender_rate(app_tab,app_id)
+		age_pie = age_distribution(app_tab,app_id)
+		context = dict(
+			host=REMOTE_HOST,
+			myechart_gender = gender_pie.render_embed(),
+			script_list_gender=gender_pie.get_js_dependencies(),
+			myechart_age=age_pie.render_embed(),
+			script_list_age=age_pie.get_js_dependencies()
+		)
+		return HttpResponse(template.render(context, request))
+	return render_to_response('charts/pyecharts.html')
+def get_name(id):
+    return app_tab.loc[id,'name']
 
 def gender_rate(app_tab,app_id):
     gender_data = app_tab.loc[[app_id],['male','female']]
